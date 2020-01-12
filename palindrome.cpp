c9ef6a7e34c3b6b7b34aecfd1e9a75f71b4b5563
weiss21 <waisalirobleh@gmail.com>
2020-01-12 22:32:33 +0000

    first file

---
+++ palindrome.cpp
@@ -0,0 +1,12 @@
+/*
+ * Title: palindrome.cpp
+ * Abstract: This program reads a text file and shows the min distance between two numbers.
+ * Author: Wais Robleh
+ * ID: 1624
+ * Date: 01/10/2020
+ */
+ 
+ #include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
