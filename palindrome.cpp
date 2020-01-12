99e8223e7fb8a3f06bb791f6cb75aa15ac991745
weiss21 <waisalirobleh@gmail.com>
2020-01-12 23:01:10 +0000

    has char array

--- palindrome.cpp
+++ palindrome.cpp
@@ -6,7 +6,25 @@
  * Date: 01/10/2020
  */
  
- #include <iostream>
-#include <fstream>
+#include <iostream>
 #include <string>
+#include <cstring>
 #include <cmath>
+using namespace std;
+
+int main() {
+    
+    string input;
+    
+    cout << "Input string: ";
+    cin >> input;
+    
+    char copystr[input.size() + 1]; //char array for input string. Has to be after input is chosen.
+    strcpy(copystr, input.c_str()); //copying string into character array
+    cout << "\ninput is: " << copystr << endl;
+    cout << "\narray count is: " << sizeof(copystr) << endl;
+    
+
+    
+    return 0;
+}
