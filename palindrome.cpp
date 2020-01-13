cb0139ce580ab1f7a5f43e0601c59d64bf5fefa2
weiss21 <waisalirobleh@gmail.com>
2020-01-13 00:04:15 +0000

    before asking

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,6 +1,6 @@
 /*
  * Title: palindrome.cpp
- * Abstract: This program reads a text file and shows the min distance between two numbers.
+ * Abstract: This program determines if input string is a palindrome or not.
  * Author: Wais Robleh
  * ID: 1624
  * Date: 01/10/2020
@@ -19,12 +19,34 @@ int main() {
     cout << "Input string: ";
     cin >> input;
     
-    char copystr[input.size() + 1]; //char array for input string. Has to be after input is chosen.
+    char copystr[input.size()]; //char array for input string. Has to be after input is chosen.
     strcpy(copystr, input.c_str()); //copying string into character array
-    cout << "\ninput is: " << copystr << endl;
-    cout << "\narray count is: " << sizeof(copystr) << endl;
     
+    int front, back;
+    back = strlen(copystr) - 1;
+    
+    cout << "\nfront is  is: " << front << endl;
+    cout << "\nback is: " << back << endl;
+    while(front <= back){
+        
+        if(copystr[front] == copystr[back]){
+            back--;
+            front++;
+        }
+        else { 
+            break;
+        }
+    }
+    
+    //cout << "\ninput is: " << copystr << endl;
+    //cout << "\narray count is: " << sizeof(copystr) << endl;
+    cout << "\nfront is  is: " << front << endl;
+    cout << "\nback is: " << back << endl;
 
-    
+    if (front > back) 
+        cout << "Yes, it is a Palindrome" << endl; 
+    else
+        cout << "No, it is not a Palindrome" << endl; 
+        
     return 0;
 }
